96e105fbeea40e9b7f4de49de8d606ced42e286a
Marco.seb <marco.seba96@gmail.com>
2020-12-16 15:22:43 +0100

    upgrade

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert('');
+
+var giocatoreReale = Math.floor(Math.random()*6) +1;
+console.log(giocatoreReale);
+
+var giocatoreCpu = Math.floor(Math.random()*6) +1;
+console.log(giocatoreCpu);
+
+var giocatoreReale = giocatoreCpu;
+var message = parità;
+
+if (giocatoreReale > giocatoreCpu) {
+    message = vittoria
+} else if (giocatoreReale < giocatoreCpu){
+    message = lose ;
+}
+
+console.log(message);
