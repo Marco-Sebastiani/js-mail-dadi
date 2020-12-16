78dc714ebb2b45cd8bb53df0a2723b0ad7723b84
Marco.seb <marco.seba96@gmail.com>
2020-12-16 16:07:34 +0100

    integration btn

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-
+/* 
 var giocatoreReale = Math.floor(Math.random()*6) +1;
-/* console.log(giocatoreReale);
- */
+
 var giocatoreCpu = Math.floor(Math.random()*6) +1;
-/* console.log(giocatoreCpu);
- */
+
 
 var message = 'pari';
 
@@ -14,9 +12,41 @@ if (giocatoreReale > giocatoreCpu) {
     message = 'La cpu ha vinto';
 }
 
-/* console.log(message);
- */
+
 document.getElementById('my-profyle').innerHTML += giocatoreReale;
 document.getElementById('cpu').innerHTML += giocatoreCpu;
-document.getElementById('mex').innerHTML = message;
+document.getElementById('mex').innerHTML = message; */
 
+var element = document.getElementById("gioca");
+element.addEventListener('click',
+    function(){
+        var giocatoreReale = Math.floor(Math.random()*6) +1;
+
+        var giocatoreCpu = Math.floor(Math.random()*6) +1;
+
+
+        var message = 'pari';
+
+        if (giocatoreReale > giocatoreCpu) {
+        message = 'Il giocatore ha vinto'
+        } else if (giocatoreReale < giocatoreCpu){
+        message = 'La cpu ha vinto';
+        }
+
+
+        document.getElementById('my-profyle').innerHTML += giocatoreReale;
+        document.getElementById('cpu').innerHTML += giocatoreCpu;
+        document.getElementById('mex').innerHTML += message;
+    }
+);
+
+
+var resetta = document.getElementById("reset");
+resetta.addEventListener('click',
+    function() {
+        document.getElementById('my-profyle').innerHTML += ' ';
+        document.getElementById('cpu').innerHTML += ' ';
+        document.getElementById('mex').innerHTML = ' ';
+
+    }
+);
